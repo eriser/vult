59155e571b3287d29b77ea4a1d0b713eabcd0fb8
Leonardo <modlfo@gmail.com>
2016-07-10 20:37:50 +0200

    Check if llvm can be just installed

--- .travis-ocaml.sh
+++ .travis-ocaml.sh
@@ -83,8 +83,6 @@ install_on_linux () {
     sudo add-apt-repository "${TRUSTY}"
     sudo apt-get -qq update
   fi
-  wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
-  deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.8 main
   sudo apt-get update
   sudo apt-get install clang-3.8
 }
